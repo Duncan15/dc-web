91895eab59a73a974785f7ffec29bcf4909d7bbf
Chang Liu <2036334551@qq.com>
2019-11-01 19:03:04 +0800

    Signed-off-by: Chang Liu <2036334551@qq.com>

--- dc-backend/src/main/java/api/estimate/EstimateServlet.java
+++ dc-backend/src/main/java/api/estimate/EstimateServlet.java
@@ -98,6 +98,7 @@ public class EstimateServlet extends HttpServlet {
             String estiId="";
             try {
                 estiId = pathParam[2];
+
                 System.out.println("estiId is "+estiId);
 //                Logger.getLogger("estiId ",estiId);
             }catch (NullPointerException nu){
